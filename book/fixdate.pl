4807929a6198f1a83d7073a5c53e7e313b87bded
Marcin Serwin <marcin.serwin0@protonmail.com>
2022-05-13 22:48:38 +0200

    Display commit sha in artifact PDFs

--- book/fixdate.pl
+++ book/fixdate.pl
@@ -1,6 +1,14 @@
-chomp ($d = `date +"%B %d, %Y"`);
-$v = pop @ARGV;
-while (<>) {
- s/^Version.+/Version~$v, $d/;
- print;
+use strict;
+use warnings;
+
+chomp (my $d = `date +"%B %d, %Y"`);
+my ($v, $sha) = @ARGV;
+if (defined $sha) {
+  $sha = substr $sha, 0, 7;
+}
+
+while (<STDIN>) {
+  s/^Version.+/Version~$v, $d/ if not (defined $sha);
+  s/^Version.+/Nightly version~$v\@$sha, $d/ if (defined $sha);
+  print;
 }
