63c09b2684a93622b721613593c89a356149206f
Marcin Serwin <marcin.serwin0@protonmail.com>
2022-06-12 17:07:18 +0200

    Add copyright notices to source files

--- book/fixdate.pl
+++ book/fixdate.pl
@@ -1,3 +1,10 @@
+# Copyright (C) 2022 Tobias Oetiker, Marcin Serwin
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
 use strict;
 use warnings;
 
